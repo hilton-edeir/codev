eb7b5a7f965f94088e3b1ad26e6be991ed6f3bf0
Hilton Tavares <hilton.edeir@gmail.com>
2024-12-13 22:49:29 +0000

    update

--- assets/js/main2.js
+++ assets/js/main2.js
@@ -1,9 +1,17 @@
 
 /*------------------------------ Loader ------------------------------*/
 window.addEventListener("load", ()=>{
+
     const loader = document.querySelector(".loader");
     loader.classList.add("loader--hidden");
+    
     loader.addEventListener("transitionend", () => {
         document.body.removeChild("loader");
-    })
+    });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const today = new Date();
+    const year = today.getFullYear();
+    document.getElementById('today-year').innerHTML = year;
 });
